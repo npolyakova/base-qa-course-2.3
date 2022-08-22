e8b2ad521078823bf215c3102af3166d288a5840
Natalia <67316260+npolyakova@users.noreply.github.com>
2022-08-22 19:01:51 +0300

    Update Task3.java

--- src/Task3.java
+++ src/Task3.java
@@ -1,6 +1,6 @@
 public class Task3 {
 
-    //Найти длину окружности радиуса 4
+    //РќР°Р№С‚Рё РґР»РёРЅСѓ РѕРєСЂСѓР¶РЅРѕСЃС‚Рё СЂР°РґРёСѓСЃР° 4
     public static void main(String[] args) {
         int r = 4;
         double l = 2 * Math.PI * r;
